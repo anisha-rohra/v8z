ae03b0cb5e5b374ad1a3494430e2902a917b6277
Anisha Rohra <Anisha.Rohra@ibm.com>
2017-05-10 12:06:10 -0400

    include path checks for -I paths non-recursively

--- tools/anisha/ebcdic2ascii.py
+++ tools/anisha/ebcdic2ascii.py
@@ -1,149 +1,54 @@
 #!/usr/bin/env python
 
 import re, sys, optparse, read_files
-
-# This is a utility for converting literals in V8 source code from
-# EBCDIC encoding to ASCII.  
-OPEN_PAREN          = "(\()"
-CLOSE_PAREN         = "(\))"
-NEWLINE             = "(\\n)"
-COMMA               = "(,)"
-OUTSTREAM_OP        = "(<<)"
-UNICODE_PRE         = "u8"
-PRINT_FUNCTIONS     = "(PrintError|printf|PrintF|VFPrintF|SPrintF|sprintf|vfprintf|feprintf|FePrintF|open)"
-USTR_MACRO          = "(USTR)"         
-STRING              = '(".*?(?<!\\\\)")'
-CHAR                = "('.{1,2}')"
-HEX_CHAR            = "('\\\\x[0-9A-Fa-f]{1,2}')"
-IGNORE_STRING       = "#\s*include\s*<.*>|#\s*pragma|\s*//|extern\s+\"C\""
-INCLUDE_STRING      = '#\s*include\s*".*"'
-HEX_ENCODED_STRING  = r'(?:\\x[0-9A-Fa-f]{1,2})+'
-CONCAT              = "(##)"
-STRINGIFY           = "(#{1}\w+)"
-WHITESPACE          = "(\s*)"
-SPLIT_TOKEN_LIST    = [ PRINT_FUNCTIONS, USTR_MACRO, COMMA, NEWLINE, UNICODE_PRE, OPEN_PAREN, CLOSE_PAREN, OUTSTREAM_OP, STRING, CHAR, HEX_CHAR,  CONCAT, STRINGIFY]
-
-EBCDIC_PRAGMA_START  = re.compile(r'\s*#pragma\s+convert\s*\(\s*\"IBM-1047\"\s*\)|\s*#pragma\s+convert\s*\(\s*\"ibm-1047\"\s*\)')
-EBCDIC_PRAGMA_END    = re.compile(r"\s*#pragma\s+convert\s*\(\s*pop\s*\)")
-MULTILINE_COMMENT_START  = re.compile(r"^\s*/\*")
-MULTILINE_COMMENT_END    = re.compile(r".*\*/\s*")
-
-#ignore lines starting with
-IGNORE_RE = re.compile(IGNORE_STRING)
-INCLUDE_RE = re.compile(INCLUDE_STRING)
-
-#C-string literals in the source
-DEFINE_RE             = re.compile(r'#pragma|#import|#error|#define|#undef|#endif|#if|#ifdef|#else|#elseif|#elif')
-OUTSTREAM_OP_RE       = re.compile(OUTSTREAM_OP)
-CLOSE_PAREN_RE        = re.compile(CLOSE_PAREN)
-STRING_RE             = re.compile(STRING)
-CHAR_RE               = re.compile(CHAR)
-NEWLINE_RE            = re.compile(NEWLINE)
-HEX_ENCODED_STRING_RE = re.compile(HEX_ENCODED_STRING) 
-PRINT_FUNCTIONS_RE    = re.compile(PRINT_FUNCTIONS)
-USTR_MACRO_RE         = re.compile(USTR_MACRO)
-STRINGIFY_RE          = re.compile(STRINGIFY)
-OPEN_PAREN_RE         = re.compile(OPEN_PAREN)
-CLOSE_PAREN_RE        = re.compile(CLOSE_PAREN)
-SPLIT_RE              = re.compile(('|').join(SPLIT_TOKEN_LIST))
-
-#TOKENIZER FOR string literal
-ESCAPE_RE    = re.compile(r'\\n|\\t|\\v|\\r|\\f|\\a|\\b|\\\'|\\\"|\\\\|\\0')
-HEX_RE       = re.compile(r"(\\x[0-9A-Fa-f]{1,2})")
-OCTAL_RE     = re.compile(r"(\\[0-7]{1,3})")
-UNICODE_RE1  = re.compile(r"(\\u\[0-9A-Fa-f]{1,4})")
-UNICODE_RE2  = re.compile(r"(\\U\[0-9A-Fa-f]{1,8})")
-ENCODING_RE  = re.compile(r"(\\x[0-9A-Fa-f]{1,2} |\\\\[0-7]{1,3} |\\\\u\[0-9A-Fa-f]{1,4} |\\\\U\[0-9A-Fa-f]{1,8})")
-
-PRINTF_RE=re.compile('%{1}\s*[-+#0]*\s*[0-9]*[.]*[0-9]*[hljztL]*[iduoxXffFeEgGaAcspn]+')
-
-
-#CONVERSION TABLES
-ESCAPE_LIT = {"\\n":'\n', "\\t":'\t', "\\v":'\v', "\\r":'\r',  "\\f":'\f',
-              "\\a":'\a', "\\b":'\b', "\\'":'\'', "\\\"":'\"', "\\\\":'\\',
-              "\\0":'\0'}
-
-EBCDIC_TO_ASCII = [
-0x00, 0x01, 0x02, 0x03, 0xff, 0x09, 0xff, 0x7f, 0xff, 0xff, 0xff, 0x0b, 0x0c, 
-0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0xff, 0x0a, 0x08, 0xff, 0x18, 0x19, 
-0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1c, 0xff, 0xff, 0x0a, 0x17, 
-0x1b, 0xff, 0xff, 0xff, 0xff, 0xff, 0x05, 0x06, 0x07, 0xff, 0xff, 0x16, 0xff, 
-0xff, 0xff, 0xff, 0x04, 0x14, 0x15, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1a, 0x20, 
-0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x2e, 0x3c, 0x28, 
-0x2b, 0x7c, 0x26, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x21, 
-0x24, 0x2a, 0x29, 0x3b, 0x5e, 0x2d, 0x2f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
-0xff, 0xff, 0xff, 0x2c, 0x25, 0x5f, 0x3e, 0x3f, 0xff, 0xff, 0xff, 0xff, 0xff, 
-0xff, 0xff, 0xff, 0xff, 0x60, 0x3a, 0x23, 0x40, 0x27, 0x3d, 0x22, 0xff, 0x61, 
-0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0xff, 0xff, 0xff, 0xff, 0xff, 
-0xff, 0xff, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70, 0x71, 0x72, 0xff, 0xff, 
-0xff, 0xff, 0xff, 0xff, 0xff, 0x7e, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 
-0x7a, 0xff, 0xff, 0xff, 0x5b, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
-0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x5d, 0xff, 0xff, 0x7b, 0x41, 0x42, 
-0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 
-0x7d, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f, 0x50, 0x51, 0x52, 0xff, 0xff, 0xff, 
-0xff, 0xff, 0xff, 0x5c, 0xff, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 
-0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 
-0x37, 0x38, 0x39, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-
-def EncodeInEBCDIC(literal):
-   convert = "";
-   for byte in literal:
-      hex_lit = str(hex(ord(byte)))
-      convert = convert + '\\x' + hex_lit[2:4]
-   return convert 
-
-def EncodeInASCII(literal):
-   convert = "";
-   for byte in literal:
-      ascii_lit  = str(hex(EBCDIC_TO_ASCII[ord(byte)]))
-      convert = convert + '\\x' + ascii_lit[2:4]
-   return convert 
-
-def ConvertMacroArgs(token):
-   if DEFINE_RE.match(token):
-      return token
-   if STRINGIFY_RE.match(token): 
-      token = token.strip()
-      return " USTR("+token+")"
-   return token
-
-def ConvertTokens(tokens):
-   if not HEX_RE.search(tokens):
-      return EncodeInASCII(tokens)
-   else:
-      return tokens
-
-def EncodeEscapeSeq(literal):
-   return EncodeInASCII(ESCAPE_LIT[literal.group(0)])
-
-def EncodeChars(literal):
-   return EncodeInASCII(literal.group(0))
-
-def EncodePrintF(literal):
-   return EncodeInEBCDIC(literal.group(0))
+from ebcdic2ascii_encoder import *
 
 # takes in a file list and parameters for unicode encoding and skipping print strings
 # intended for use by other python programs, not for scripts for commandline
-def open_files(file_list, unicode_encode=False, skip_print_strings=False):
+def open_files(file_list, unicode_encode=False, skip_print_strings=False, include_paths=[]):
 
    # error-check: need exactly two file paths in file_list
    if len(file_list) != 2:
       print("ERROR: Source and target file path required.")
       return 1
 
-   convert_to_ascii(file_list, unicode_encode, skip_print_strings)
+   convert_to_ascii(file_list, unicode_encode, skip_print_strings, include_paths)
 
    # resets the global blacklist contained in read_files.py for header files
    read_files.reset_blacklist()
    return 0
 
+def make_delimiters(tokens_of_interest, skip_print_strings):
+   delimiters = []
+   delete = False
+   open_paren = 0
+   start_index = 0
+   stop_index  = 0
+
+   for index in range(len(tokens_of_interest)):
+      token = tokens_of_interest[index]
+      if USTR_MACRO_RE.match(token):
+         start_index = index
+         delete = True
+      if skip_print_strings and PRINT_FUNCTIONS_RE.match(token):
+         start_index = index
+         delete = True
+      if delete and OPEN_PAREN_RE.match(token):
+         open_paren = open_paren + 1;
+      if delete and (CLOSE_PAREN_RE.match(token) or NEWLINE_RE.match(token)):
+         open_paren = open_paren - 1; 
+         if open_paren == 0:
+            stop_index = index
+            delimiters.append((start_index, stop_index))
+
+   return delimiters
+
 # main function to convert from ebcdic to ascii
-def convert_to_ascii(filenames, unicode_encode, skip_print_strings):
+def convert_to_ascii(filenames, unicode_encode, skip_print_strings, include_paths):
    Source          = open(filenames[0], "rt")
    Target          = open(filenames[1], "at+")
 
    # flags to determine exactly what the line of code in the source contains
-
    ebcdic_encoding = False
    convert_start = False
    convert_end = False
@@ -155,14 +60,20 @@ def convert_to_ascii(filenames, unicode_encode, skip_print_strings):
    skip_line = False 
    include_line = False
 
-   #Main loop which identifies and encodes literals with hex escape sequences
+   # main loop which identifies and encodes literals with hex escape sequences
    for line in Source:
+      
+      # check if the line is a comment
       comment_start = MULTILINE_COMMENT_START.match(line)
-      convert_start = EBCDIC_PRAGMA_START.match(line)
       multiline_comment = (multiline_comment or comment_start)\
        and (not comment_end)
+
+      # check if conversion is requested
+      convert_start = EBCDIC_PRAGMA_START.match(line)
       ebcdic_encoding = (ebcdic_encoding or convert_start)\
        and (not convert_end)
+
+      # check if the line should be ignored
       skip_line = IGNORE_RE.match(line)\
        or multiline_comment or ebcdic_encoding 
       include_line = INCLUDE_RE.match(line)
@@ -171,28 +82,8 @@ def convert_to_ascii(filenames, unicode_encode, skip_print_strings):
          tokens_of_interest = re.split(SPLIT_RE, line)
          tokens_of_interest = filter(None, tokens_of_interest)
 
-         #Mark strings inside functions and between outstream_op which we do not want to be modified 
-         delimiters = []
-         delete = False
-         open_paren = 0
-         start_index = 0
-         stop_index  = 0
-
-         for index in range(len(tokens_of_interest)):
-            token = tokens_of_interest[index]
-            if USTR_MACRO_RE.match(token):
-               start_index = index
-               delete = True
-            if skip_print_strings and  PRINT_FUNCTIONS_RE.match(token):
-               start_index = index
-               delete = True
-            if delete and OPEN_PAREN_RE.match(token):
-               open_paren = open_paren + 1;
-            if delete and (CLOSE_PAREN_RE.match(token) or NEWLINE_RE.match(token)):
-               open_paren = open_paren - 1; 
-               if open_paren == 0:
-                  stop_index = index
-                  delimiters.append((start_index, stop_index))
+         # mark strings inside functions and between outstream_op which we do not want to be modified 
+         delimiters = make_delimiters(tokens_of_interest, skip_print_strings)
 
          index = 0;
          newline = ""
@@ -200,8 +91,6 @@ def convert_to_ascii(filenames, unicode_encode, skip_print_strings):
          start_index = -1;
          stop_index  = -1;
          ostream_string = False
-         #print tokens_of_interest
-         #print delimiters
 
          while index < (len(tokens_of_interest)):
             token = tokens_of_interest[index];
@@ -264,13 +153,55 @@ def convert_to_ascii(filenames, unicode_encode, skip_print_strings):
             index = index + 1;
          line = newline;     
 
-      # MINE
+      # if the line is an #include statement
       if include_line:
          print(line)
-         HEADER_RE = re.compile('#\s*include "(.*)"')
-         h = HEADER_RE.search(line).group(1)
-         target_header = read_files.main([h, filenames[0]])
-         Target.write('#include "' + target_header + '"\n')
+         target_header = 0
+         
+         # check if its an absolute path, as those take priority
+         ABSOLUTE_RE = re.compile('#\s*include "(/.*)"')
+         a = ABSOLUTE_RE.search(line)
+         if a is not None:
+            absolute_path = a.group(1)
+            print(absolute_path)
+            target_header = read_files.main([absolute_path, ''])
+            Target.write('#include "' + target_header + '"\n')
+         
+         else:
+            # check if its a local relative path
+            HEADER_RE = re.compile('#\s*include "(.*)"')
+            h = HEADER_RE.search(line)
+            if h is not None:
+               header_path = h.group(1)
+               target_header = read_files.main([header_path, filenames[0]])         
+               if target_header == 1:
+                  print("no relative path exists")
+                  for path in include_paths:
+                     print(path)
+                     target_header = read_files.main([header_path, path])
+                     if target_header != 1:
+                        break
+               Target.write('#include "' + target_header + '"\n')
+           
+            # it must be a <> include path, need to check if its in the -I include paths
+            else:
+               INCLUDE_BRACKETS_RE = re.compile('#\s*include <(.*)>')
+               i = INCLUDE_BRACKETS_RE.search(line)
+               include_brackets_path = i.group(1)
+               print(include_brackets_path)
+               for path in include_paths:
+                  print(path)
+                  target_header = read_files.main([include_brackets_path, path])
+                  if target_header != 1:
+                     break
+
+               # if it can't be found in the <> include path, assume it is a standard library and replace with that
+               if target_header == 1 or target_header == 0:
+                  target_header = include_brackets_path
+
+               Target.write('#include <' + target_header + '>\n')
+
+         print(target_header)
 
       else:
          comment_end = MULTILINE_COMMENT_END.match(line)
@@ -288,23 +219,25 @@ def parse_arguments():
    # parse the command line arguments, looking for potential flags -u and --skip_print
    # along with two file paths, input and output
    parser = optparse.OptionParser()
-   parser.set_usage("""ebcdic2ascii.py [options] input.cc output.cc 
+   parser.set_usage("""ebcdic2ascii.py [options] input.cc output.cc include_paths
    input.cc: File to be converted 
    output.cc: Converted File.""")
    parser.add_option("-u", action="store_true", dest="unicode_support", default = False, help="convert strings using u8 prefix")
    parser.add_option("--skip_print", action="store_true", dest="skip_print_strings", default = False, help="skip strings going to snprtinf,printf,output stream")
+   parser.add_option("-I", action="append", dest="include_paths", default=[], help="provide another root path to look at for #include statements")
 
    (options, args) = parser.parse_args()
 
    # error-check: two arguments from command-line expected
-   if len(args) != 2:
+   if len(args) < 2:
       print("ERROR: Source and target file path required.")
       return 1
 
    unicode_encode             = options.unicode_support;
    skip_print_strings         = options.skip_print_strings; 
 
-   convert_to_ascii(args, unicode_encode, skip_print_strings)
+   print(options.include_paths)
+   convert_to_ascii(args[:2], unicode_encode, skip_print_strings, options.include_paths)
 
    # resets the global blacklist contained in read_files.py for header files
    read_files.reset_blacklist()
--- tools/anisha/read_files.py
+++ tools/anisha/read_files.py
@@ -1,4 +1,4 @@
-import ebcdic2ascii, sys, re
+import ebcdic2ascii, sys, re, os
 
 blacklist = []
 
@@ -11,32 +11,47 @@ def reset_blacklist():
 	blacklist = []
 
 def main(args):
+	print(args)
 
 	# args contains the target path which is the path to the header that needs to be
 	# translated and the source path which is the path to the original source file
+	
+	# need the relative directory path of the source file
 	expr1 = re.compile("(.*)/.*")
 	source = expr1.match(args[1]).group(1)
-
-	expr2 = re.compile("(.*)\.(.*)")
-	part1 = expr2.match(args[0]).group(1)
-	part2 = expr2.match(args[0]).group(2)
-
+	
 	# source header_path is the path to be translated
+	source_header_path = source + "/" + args[0]
+
+	# need to say a _temp bewtween the name of the file and the file's extension
+	expr2 = re.compile("(.*)\.(.*)")
+	t = expr2.match(args[0]) 
+
 	# target header_path is the path where the translation is stored
 	# currently, that is the same directory as the source header path with the same
 	# name and a _temp after that name before the extension
-	source_header_path = source + "/" + args[0]
-	target_header_path = source + "/" + part1 + "_temp." + part2
+	if t is not None:
+		part1 = t.group(1)
+		part2 = t.group(2)
+		target = part1 + "_temp." + part2
+		target_header_path = source + "/" + part1 + "_temp." + part2
+	else:
+		target = args[0] + "_temp"
+		target_header_path = source + "/" + args[0] + "_temp"
 
 	# add the source header path to the blacklist to ensure the same headers aren't being
 	# translated again
 	if not header_exists(source_header_path):
+		if not os.path.isfile(source_header_path):
+			print("path is not a valid file")
+			return 1
+
 		blacklist.append(source_header_path)
 		ebcdic2ascii.open_files([source_header_path, target_header_path], False, False)
 		
 	# return the location to the target header path so that it can be written into the
 	# translated source file
-	return part1 + "_temp." + part2
+	return target
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
